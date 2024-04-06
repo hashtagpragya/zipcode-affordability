15fb08113df60bb0da634768c908ab246ca64277
usonia09 <uwasonia2001@gmail.com>
2024-04-06 19:55:33 -0400

    setup 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import mapboxgl from "mapbox-gl";
+    import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+
+    mapboxgl.accessToken = import.meta.env.VITE_MY_TOKEN;
+
+</script>
+
+<h1>Zipcode Affordability</h1>
+<div id="map"/>
+
+<input type="number" id="income-box" placeholder="Enter your annual income">
+
+<style>
+    @import url("$lib/global.css");
+    #map {
+	flex: 1;
+    background-color: aqua;
+    }
+
+    #income-box{
+        margin-top: 1em;
+        padding: 0.5em;
+        width: 20em;
+        border-radius: 5px;
+    }
+
+</style>
